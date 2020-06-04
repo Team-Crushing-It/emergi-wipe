bc94e66ca301448ec95e9c40a8bc48c63d91ec7e
AgrawalTrisha <agrawaltrisha@gmail.com>
2020-06-04 12:38:32 -0500

    created w/ stful

---
+++ lib/speed_manager.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SpeedManager extends StatefulWidget {
+  @override
+  _SpeedManagerState createState() => _SpeedManagerState();
+}
+
+class _SpeedManagerState extends State<SpeedManager> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
