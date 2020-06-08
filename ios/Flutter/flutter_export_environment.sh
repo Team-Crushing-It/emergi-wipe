05edecc588086d4a08037017b741e2259f97df23
Gene-Dana <yllanesgene@gmail.com>
2020-06-08 19:54:25 -0400

    some updates

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/gene/Dev/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/gene/Documents/dev/code/instaplant/flutter-bluettoth"
+export "FLUTTER_APPLICATION_PATH=/Users/gene/Documents/dev/code/emergency-wipe"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
