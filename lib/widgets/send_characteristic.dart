fab5f85969555517500ae87c1583f1ec39fdeee6
mihirKachroo <60433268+mihirKachroo@users.noreply.github.com>
2020-06-10 13:39:00 -0400

    send to characteristics added

--- lib/widgets/send_characteristic.dart
+++ lib/widgets/send_characteristic.dart
@@ -13,57 +13,14 @@ void sendChar(int i) async{
     final BluetoothDevice device=globals.gdevice;
 
     List<BluetoothService> services = await device.discoverServices();
-    services.forEach((service) {
-    });
-/*    var characteristics = service.characteristics;
-
-    for(BluetoothCharacteristic c in characteristics) {
+    for(BluetoothService service in services) {
       if (service.uuid.toString() == "0000dfb0-0000-1000-8000-00805f9b34fb"){
-        if (c.uuid.toString() == "0000dfb1-0000-1000-8000-00805f9b34fb") {
-          c.write(utf8.encode(stringValue), withoutResponse: true);
-        }
-      }
-    }*/
-}
-
-
-
-
-  /*
-  @override
-void sendMessageToDevice() async {
-  print('ninot');
-  print(characteristic);
-}
-FlutterBlue.instance.connectedDevices;
-List<BluetoothService> services = await device.discoverServices();
-services.forEach((service) {
-    // do something with service
-});
-
-    for(BluetoothCharacteristic c in characteristics) {
-  if (service.uuid.toString() == "0000dfb0-0000-1000-8000-00805f9b34fb"){
-    if (c.uuid.toString() == "0000dfb1-0000-1000-8000-00805f9b34fb") {
-      List<int> value = await c.read();
-      if (value.toString() == "[1]"){
-        await c.write([0]);
-      }
-      else if (value.toString() == "[0]"){
-        await c.write([1]);
-      }          
-    }
-  }
-}
-*/
-
-
-/*
-for(BluetoothService service in services) {
-        for(BluetoothCharacteristic c in service.characteristics) {
-          if(c.uuid == new Guid("0000ffe1-0000-1000-8000-00805f9b34fb")) {
-            _setNotification(c);
-          } else {
-            print("Nope");
+        var characteristics = service.characteristics;
+        for(BluetoothCharacteristic c in characteristics) {
+          if (c.uuid.toString() == "0000dfb1-0000-1000-8000-00805f9b34fb") {
+            c.write(utf8.encode(stringValue), withoutResponse: true);
           }
-        }
-      }*/
+      }
+    }
+}
+}
