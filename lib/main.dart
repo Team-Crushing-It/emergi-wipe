1f84f9846ed8d628a968a153915d0c34b4841787
Gene-Dana <yllanesgene@gmail.com>
2020-06-10 10:27:03 -0400

    making it work!

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 
 import 'package:flutter_bluetooth/widgets.dart';
+import 'package:flutter_bluetooth/speed_manager.dart';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bluetooth/widgets/emergi_wipe_logo.dart';
+import 'package:flutter_bluetooth/widgets/on_off_switch.dart';
+import 'package:flutter_bluetooth/widgets/speed_bar.dart';
+import 'package:flutter_bluetooth/widgets/speed_pyramid.dart';
+import 'package:flutter_bluetooth/widgets/send_characteristic.dart';
 
 
 void main() {
@@ -100,7 +109,7 @@ class FindDevicesScreen extends StatelessWidget {
                                     onPressed: () => Navigator.of(context).push(
                                         MaterialPageRoute(
                                             builder: (context) =>
-                                                DeviceScreen(device: d))),
+                                                SpeedManager(device: d))),
                                   );
                                 }
                                 return Text(snapshot.data.toString());
@@ -121,7 +130,7 @@ class FindDevicesScreen extends StatelessWidget {
                           onTap: () => Navigator.of(context)
                               .push(MaterialPageRoute(builder: (context) {
                             r.device.connect();
-                            return DeviceScreen(device: r.device);
+                            return SpeedManager(device: r.device);
                           })),
                         ),
                       )
@@ -156,15 +165,103 @@ class FindDevicesScreen extends StatelessWidget {
 
 //================================================================================================
 
-class DeviceScreen extends StatelessWidget {
-  const DeviceScreen({Key key, this.device}) : super(key: key);
+// class SpeedManager extends StatefulWidget {
+//   const SpeedManager({Key key, this.device}) : super(key: key);
 
-  final BluetoothDevice device;
+//   final BluetoothDevice device;
 
   
-//==========================================================================================================
+// //==========================================================================================================
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold();
+//   }
+// }
+class SpeedManager extends StatefulWidget {
+  
+  final BluetoothDevice device;
+
+  const SpeedManager({Key key, this.device} ) : super(key: key);
+
+  @override
+  _SpeedManagerState createState() => _SpeedManagerState();
+}
+
+class _SpeedManagerState extends State<SpeedManager> {
+  int speed = 0;
+  bool isOn = false;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Container(
+      color: Theme.of(context).backgroundColor,
+      child: Column(crossAxisAlignment: CrossAxisAlignment.center, mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: EmergiWipeLogo(),
+        ),
+        GestureDetector(
+          behavior: HitTestBehavior.translucent,
+          onVerticalDragEnd: (details) {
+            if (details.velocity.pixelsPerSecond.dy > 0 && speed > 1) {
+             // widget.setSpeed(speed - 1);
+              sendChar(speed-1);
+              setState(() {
+                speed -= 1;
+              });
+            } else if (details.velocity.pixelsPerSecond.dy < 0 && speed < 6) {
+            //  widget.setSpeed(speed + 1);
+              sendChar(speed+1);
+              setState(() {
+                speed += 1;
+              });
+            }
+          },
+          child: IntrinsicHeight(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [Icon(Icons.arrow_upward), Text("Swipe", style: TextStyle(fontSize: 18)), Icon(Icons.arrow_downward)],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 4,
+                    child: SpeedPyramid(speed: speed, isOn: isOn),
+                  ),
+                  Spacer(),
+                ],
+              ),
+            ),
+          ),
+        ),
+        OnOffSwitch(
+            isOn: isOn,
+            onTap: () {
+              if(isOn) {
+             //   widget.setSpeed(0);
+              } else {
+             //   widget.setSpeed(speed);
+              }
+              setState(() {
+                isOn = !isOn;
+              });
+            })
+      ]),
+    );
+  }
+}
+
+class DebugBorder extends StatelessWidget {
+  final Widget child;
+
+  const DebugBorder({this.child});
+  @override
+  Widget build(BuildContext context) {
+    return Container(decoration: BoxDecoration(border: Border.all(color: Colors.black, width: 3)), child: child);
   }
 }
