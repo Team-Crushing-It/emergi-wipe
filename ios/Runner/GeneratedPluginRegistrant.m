332cc81c7c1de4e4b87d890550c07d8ec2a29fb1
Gene-Dana <yllanesgene@gmail.com>
2020-06-10 09:24:43 -0400

    not sure what happened here but yeah

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,26 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<e2e/E2EPlugin.h>)
+#import <e2e/E2EPlugin.h>
+#else
+@import e2e;
+#endif
+
+#if __has_include(<flutter_blue/FlutterBluePlugin.h>)
+#import <flutter_blue/FlutterBluePlugin.h>
+#else
+@import flutter_blue;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [E2EPlugin registerWithRegistrar:[registry registrarForPlugin:@"E2EPlugin"]];
+  [FlutterBluePlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterBluePlugin"]];
+}
+
+@end
